f6b108458074224dd9655092ea22bf8580304d66
tanay21 <tanay21122@gmail.com>
2023-02-17 20:27:18 +0530

    1. Added Home Screen 2. Added Create Task Screen 3. Added Calender Screen.

---
+++ app/src/main/java/com/tanayjoshi/android/taskiz/CalenderScreen.java
@@ -0,0 +1,14 @@
+package com.tanayjoshi.android.taskiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalenderScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender_screen);
+    }
+}
